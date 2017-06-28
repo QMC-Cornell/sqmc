2a5a57c751c2092ef54740378cd685a603e819a7
Cyrus J. Umrigar <CyrusUmrigar@gmail.com>
2017-06-27 22:21:20 -0400

    for macs

---
+++ C2_v2z_curve/r1.0/aa.s
@@ -0,0 +1,11 @@
+for file in i*
+do
+echo $file
+ex $file <<_end
+1,\$s/.*orbital_symmetries(1:norb)/xx/
+wq
+_end
+done
+
+# 1,\$s/d2h                               point_group/dih                               point_group/
+#.s;.*;\&hf_det irreps=1,2,3,4 irrep_occs_up=3,1,1,1 irrep_occs_dn=3,1,1,1 /;
